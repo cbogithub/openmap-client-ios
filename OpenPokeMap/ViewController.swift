23c963d9682c6fe40ae68d1851137c3a308c2a0c
Jamie / Null <bishjamie@icloud.com>
2016-09-15 19:26:27 +0100

    small stuff

--- OpenPokeMap/ViewController.swift
+++ OpenPokeMap/ViewController.swift
@@ -5,9 +5,6 @@
 //  Created by Jamie Bishop on 26/08/2016.
 //  Copyright © 2016 nullpixel Development. All rights reserved.
 //
-
-import SwiftWebSocket
-import SwiftyJSON
 import UIKit
 
 class ViewController: UIViewController {
@@ -18,20 +15,6 @@ class ViewController: UIViewController {
     var timer = NSTimer()
     let internetCheckInterval = Double(5)
     var successfulTimes = 0
-
-    func checkSocket() {
-        if debug {
-            let socketURL = WebSocket("ws://192.168.0.10:8080/websocket")
-            let socketString = String(socketURL)
-            print("Socket URL is \(socketString)")
-            connectToWS(socketURL)
-        } else {
-            let socketURL = WebSocket("ws://ws.openpokemap.pw")
-            let socketString = String(socketURL)
-            print("Socket URL is \(socketString)")
-            connectToWS(socketURL)
-        }
-    }
     
     func checkInternet() {
         let status = Reach().connectionStatus()
@@ -66,7 +49,6 @@ class ViewController: UIViewController {
         var timer = NSTimer.scheduledTimerWithTimeInterval(0.4, target: self, selector: "checkInternet", userInfo: nil, repeats: true)
         
         reloadWebview()
-        checkSocket()
     }
     
     override func viewDidAppear(animated: Bool) {
@@ -88,66 +70,44 @@ class ViewController: UIViewController {
                 })
             
             alertController.show()
-        } else {
-            let alertController = UIAlertController(title: "Anyone Home?", message: "Dang. We can't call home.", preferredStyle: .Alert)
-            alertController.addAction(UIAlertAction(title: "Server Status", style: UIAlertActionStyle.Destructive) { (result : UIAlertAction) -> Void in
-                if let url = NSURL(string: "http://status.openpokemap.pw/") where UIApplication.sharedApplication().canOpenURL(url) {
-                    UIApplication.sharedApplication().openURL(url)
-                }
-                self.checkSocket()
-                })
-            alertController.addAction(UIAlertAction(title: "Retry", style: UIAlertActionStyle.Default) { (result : UIAlertAction) -> Void in
-                self.checkSocket()
-                })
-            
-            alertController.show()
         }
     }
     
-    func connectToWS(socketURL: WebSocket) {
-        var i = 1
-        let ws = socketURL
-        ws.event.open = {
-            print("Connected to socket \(ws)")
-        }
-        ws.event.close = { code, reason, clean in
-
-            if self.debug {
-                print("Couldn't connect to websocket. Start it, scrub.")
-            } else {
-                
-                if i == 5 {
-                    self.displayFailAlert(false)
-                    i = i + 1
-                } else {
-                    print("Could not connect. Retrying the \(i)'st time.")
-                    ws.open()
-                    i = i + 1
-                }
-            }
-            
-        }
-        
-        ws.event.error = { error in
-            print("error \(error)")
-        }
-        ws.event.message = { message in
-            if let text = message as? String {
-                print("recv: \(text)")
-                self.respondToChallenge(text)
-            }
-        }
-    }
-    
-    
-    func respondToChallenge(challenge: String) {
-        print("Challenge passed successfully \(challenge)")
-        let json = JSON(challenge)
-        print(json)
-        let host = json["host"].string
-        print("Host: \(host)")
-        
-    }
+//    func connectToWS(socketURL: WebSocket) {
+//        var i = 1
+//        let ws = socketURL
+//        ws.event.open = {
+//            print("Connected to socket \(ws)")
+//        }
+//        ws.event.close = { code, reason, clean in
+//
+//            if self.debug {
+//                print("Couldn't connect to websocket. Start it, scrub.")
+//            } else {
+//                
+//                if i == 5 {
+//                    self.displayFailAlert(false)
+//                    i = i + 1
+//                } else {
+//                    print("Could not connect. Retrying the \(i)'st time.")
+//                    ws.open()
+//                    i = i + 1
+//                }
+//            }
+//            
+//        }
+//        
+//        ws.event.error = { error in
+//            print("error \(error)")
+//        }
+//        ws.event.message = { message in
+//            if let text = message as? String {
+//                print("recv: \(text)")
+//                self.respondToChallenge(text)
+//            }
+//        }
+//    }
+//    
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
